d631df610f11e1caa2513e5f0570f9ba3e1decef
Nir Elbaz <nirelbaz@Nirs-MacBook-Pro.local>
2014-12-27 13:12:05 +0200

    Structure

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,6 +1,6 @@
 (function (chrome) {
   chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('../views/index.html', {
+    chrome.app.window.create('/views/index.html', {
       'bounds': {
         'width': 400,
         'height': 500
