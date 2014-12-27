ba288d093c8cfca9ef080f47f8cf6fee4b090a05
Nir Elbaz <nirelbaz@Nirs-MacBook-Pro.local>
2014-12-27 12:32:19 +0200

    My first commit

---
+++ app/scripts/background.js
@@ -0,0 +1,10 @@
+(function (chrome) {
+  chrome.app.runtime.onLaunched.addListener(function() {
+    chrome.app.window.create('../views/index.html', {
+      'bounds': {
+        'width': 400,
+        'height': 500
+      }
+    });
+  });
+})(chrome);
